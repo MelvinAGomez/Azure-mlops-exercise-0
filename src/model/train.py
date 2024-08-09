a1e9dd78057068a5375a5202a7820b68ea28ab4c
MelvinAGomez <melvin12gomez10@gmail.com>
2024-08-09 15:08:08 +0530

    challenge4 workflow 4

--- src/model/train.py
+++ src/model/train.py
@@ -40,7 +40,7 @@ def split_data(df, test_size=0.2):
     X = df.drop("Diabetic", axis=1)
     y = df["Diabetic"]
     X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=test_size
+        X, y, test_size=test_size
     )
 
     return X_train, X_test, y_train, y_test
@@ -52,7 +52,6 @@ def train_model(reg_rate, X_train, X_test, y_train, y_test):
         C=1/reg_rate,
         solver="liblinear"
     ).fit(X_train, y_train)
-
 
 
 def parse_args():
